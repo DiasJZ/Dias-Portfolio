99af1a8d4ea95fb7ae78882f507b92a344551507
João Vitor Dias <joaovdias55@gmail.com>
2023-10-12 20:31:30 -0300

    Transição React

---
+++ Dias-Portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import './Components/Home'
+
+function App() {
+  
+
+  return (
+    <>
+      {Home}
+    </>
+  )
+}
+
+export default App
---
+++ Dias-Portfolio/src/Components/Home.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+function Home(e) {
+    <div>
+        <header id="header-container">
+        <div className="intro">
+            <h1>JOÃO V. DIAS</h1>
+            <h2>Front-End Developer</h2>
+        </div>
+        <div className="social-media">
+            <a href="https://www.linkedin.com/in/jo%C3%A3o-vitor-dias-51b54925b/" rel="_blank">Linkedin</a>
+             <a href="https://github.com/DiasZinX" rel="_blank">Github</a>
+        </div>
+        <nav className="Details">
+            <div className="logo"> 
+                <img src="Imagens/Dunimini.png" alt="Logo Dias" /> 
+            </div>
+                <a href="#">Contatos</a>
+                <a href="#">Projetos</a>
+                <a href="#">Sobre Mim</a>
+                <a href="#">Home</a>
+            <div className="perfil">
+                    <img src="Imagens/foto-dias.jpg" alt="Foto Dias" />
+            </div>
+                
+        </nav>
+    </header>
+    </div>
+}
